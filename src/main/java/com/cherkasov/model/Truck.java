e691ff317ad35b11fdf874af69f545eef405a6f0
Denis Cherkasov <denis.cherkasov.19@gmail.com>
2022-12-13 19:24:43 +0200

    LJ3-250 - test

--- src/main/java/com/cherkasov/model/Truck.java
+++ src/main/java/com/cherkasov/model/Truck.java
@@ -1,13 +1,14 @@
 package com.cherkasov.model;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Data
 @Getter
 @Setter
-public class Truck extends Car implements CountRestore {
+public class Truck extends Car {
     private int loadCopacity;
 
     public Truck(String manufacturer, Engine engine, Color color, int loadCopacity) {
@@ -26,4 +27,15 @@ public class Truck extends Car implements CountRestore {
         setCount(50);
         System.out.println(getCount());
     }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
 }
